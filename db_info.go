a5a1b74f8572cd0708a368c27ed1c550c14d62fd
Iampersoncool <86845476+Iampersoncool@users.noreply.github.com>
2024-07-25 19:45:11 -0700

    Update: rename DatabaseUrlKey to EnvDatabaseUrlKey

--- db_info.go
+++ db_info.go
@@ -2,8 +2,10 @@ package main
 
 import "os"
 
+const EnvDatabaseUrlKey = "DB_URL"
+
 func GetDbInfoFromEnv(defConnUrl string) string {
-	url := os.Getenv("DB_URL")
+	url := os.Getenv(EnvDatabaseUrlKey)
 
 	if url == "" {
 		return defConnUrl
